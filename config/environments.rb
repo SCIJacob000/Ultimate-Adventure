db23ce7d02138d03e8d097068f34912b74595b9b
Jacob Stolberg <jacobstolberg@Jacobs-Air.lan>
2019-06-10 01:21:50 -0500

    fix

--- config/environments.rb
+++ config/environments.rb
@@ -18,10 +18,10 @@ configure :production do
 
 	ActiveRecord::Base.establish_connection(
 		:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-		# :host => db.host,
-		# :username => db.user,
-		# :password => db.password,
-		# :database => db.path[1..-1],
+		:host => db.host,
+		:username => db.user,
+		:password => db.password,
+		:database => db.path[1..-1],
 		:encoding => 'utf8'
 		)
 end
