8239dfdbe2367305cad56a19f422e0eeeac4406e
Jacob Stolberg <jacobstolberg@Jacobs-Air.lan>
2019-06-10 01:09:32 -0500

    fix

--- config/environments.rb
+++ config/environments.rb
@@ -11,3 +11,17 @@ configure :development do
 		:encoding => 'utf8'
 		)
 end
+
+configure :production do 
+
+	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/ultimate_adventure')
+
+	ActiveRecord::Base.establish_connection(
+		:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+		# :host => db.host,
+		# :username => db.user,
+		# :password => db.password,
+		# :database => db.path[1..-1],
+		:encoding => 'utf8'
+		)
+end
