82173db82543d5ec3e40b8bf7e120d775559a8ac
Jacob Stolberg <jacobstolberg@Jacobs-Air.lan>
2019-07-12 14:54:08 -0500

    All pages fully responsive besides map page

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   set :views, File.expand_path('../../views', __FILE__)
 
-  set :public_dir, File.expand_path('../../public', __FILE__)
+  set :public_dir, File.expand_path('../../Public', __FILE__)
 
 #splash page route
   get '/' do    
